ccd970caed13c86e0d098aa43ac2625ff4b94928
Sadan Iqbal <sadaniqbal@gmail.com>
2020-04-29 21:28:49 -0700

    Chap 9 part 1 completed

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -6,15 +6,18 @@ import { HttpClientModule } from "@angular/common/http";
 import { Order } from "./order.model";
 import { OrderRepository } from "./order.repository";
 import { RestDataSource } from "./rest.datasource";
+import { AuthService } from "./auth.service";
 
 @NgModule({
   imports: [HttpClientModule],
   providers: [
     ProductRepository,
     { provide: StaticDataSource, useClass: RestDataSource },
+    RestDataSource,
     Cart,
     Order,
     OrderRepository,
+    AuthService,
   ],
 })
 export class ModelModule {}
