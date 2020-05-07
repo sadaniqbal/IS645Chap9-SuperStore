7c89e98d1c1e5fc7f95bb30e275b214311614c04
Sadan Iqbal <sadaniqbal@gmail.com>
2020-05-07 01:18:42 -0700

    Declared searchTerm in store.component

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -18,21 +18,14 @@ export class StoreComponent {
     public cart: Cart,
     public router: Router
   ) {}
-  searchTerm: string;
+  searchTerm: string = "";
 
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
     return this.repository
       .getProducts(this.selectedCategory)
-
-      .filter(
-        (product) =>
-          product.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) !==
-          -1
-      )
       .slice(pageIndex, pageIndex + this.productsPerPage);
   }
-
   get categories(): string[] {
     return this.repository.getCategories();
   }
